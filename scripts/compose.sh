839b6baf88d75014db5b19701386fc80f9bf3b31
Lucien <lucien@lucien.ink>
2025-06-28 20:00:18 +0800

    Stash

--- scripts/compose.sh
+++ scripts/compose.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
+TMPDIR=$(mktemp -d)
+trap 'rm -rf "${TMPDIR}"' EXIT
+
 function compose() {
-    grep -v 'PWD' compose.yaml > compose.tmp.yaml
-    docker compose -f compose.tmp.yaml -f compose.dev.yaml "$@"
+    for file in compose/compose.*.yaml; do
+        if [[ -f "$file" ]]; then
+            grep "$file" -v 'PWD' > "${TMPDIR}/$(basename "$file")"
+        fi
+    done
+    grep -v 'PWD' compose.yaml > "${TMPDIR}/compose.tmp.yaml"
+    docker compose -f "${TMPDIR}/compose.tmp.yaml" -f /compose/compose.dev.yaml "$@"
 }
 
 compose "$@"
